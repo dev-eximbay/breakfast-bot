3e9c4087deea1b3f261e6f7ec133e6c92ec772de
ryeongee <ryeongee24@gmail.com>
2025-03-25 08:06:24 +0900

    feat: 조식 메뉴 업로드 예시 추가.
    - 슬랙 bolt, 통신 관련 의존성 추가
    - 명령어 사용 시, upload success! 문구 응답 추가.

--- src/main/java/org/example/breakfast/BreakfastMenuUploadApplication.java
+++ src/main/java/org/example/breakfast/BreakfastMenuUploadApplication.java
@@ -1,58 +1,34 @@
 package org.example.breakfast;
 
-import com.slack.api.methods.SlackApiException;
-import org.example.breakfast.model.Menu;
-import org.example.breakfast.service.ExcelParserService;
-import org.example.breakfast.service.FirestoreMenuService;
-import org.example.breakfast.service.SlackFileService;
+
+import com.slack.api.bolt.App;
+import com.slack.api.bolt.AppConfig;
+import com.slack.api.bolt.socket_mode.SocketModeApp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.util.List;
-
 /**
  * Created by Robin on 25. 3. 1.
- * Description : 조식 메뉴 알림 Main Application
+ * Description : 조식 메뉴 업로드 Main Application
  */
 public class BreakfastMenuUploadApplication {
     private static final Logger log = LoggerFactory.getLogger(BreakfastMenuUploadApplication.class);
 
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            log.error("파일 ID가 전달되지 않았습니다.");
-            return;
-        }
+    public static void main(String[] args) throws Exception {
+        String botToken = System.getenv("SLACK_BOT_TOKEN");
+        String appToken = System.getenv("SLACK_APP_TOKEN");
 
-        String fileId = args[0];
+        AppConfig config = AppConfig.builder()
+                .singleTeamBotToken(botToken)
+                .build();
 
-        try {
+        App app = new App(config);
 
-            // Slack에서 파일 다운로드
-            SlackFileService slackFileService = new SlackFileService();
-            String filePath = slackFileService.downloadLatestBreakfastFile(fileId, "조식-메뉴");
-            log.info("엑셀 파일 다운로드 완료: {}", filePath);
+        app.command("/upload-breakfast", (req, ctx) -> {
+            System.out.println("upload-breakfast success");
+            return ctx.ack("upload success!");
+        });
 
-            // 엑셀 파일 파싱
-            ExcelParserService excelParserService = new ExcelParserService();
-            List<Menu> menus = excelParserService.parseBreakfastMenu(filePath);
-            log.info("총 {}개의 조식 메뉴가 파싱됨", menus.size());
-
-            // Firestore 저장
-            FirestoreMenuService menuService = null;
-            try {
-                menuService = new FirestoreMenuService();
-            } catch (IOException e) {
-                log.error("Firebase 초기화 중 오류 발생", e);
-            }
-
-            if (menuService != null) {
-                menuService.saveMenus(menus);
-                log.info("Firestore에 조식 엑셀 데이터 저장 완료!");
-            }
-
-        } catch (IOException | SlackApiException e) {
-            log.error("엑셀 파일 처리 중 오류 발생", e);
-        }
+        new SocketModeApp(appToken, app).start();
     }
 }
